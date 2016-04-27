6531c4998044dfce74d00c2d84ceacc41a226b55
mpadge <mark.padgham@email.com>
2016-04-27 14:05:17 +0200

    updated tmux-start

--- tmux-start.bash
+++ tmux-start.bash
@@ -1,4 +1,10 @@
 #!/bin/sh
+if [ $# -lt 1 ]; then
+    option=1
+else
+    option=$1
+fi
+
 cd ./src/
 SESSION="osm-router"
 
@@ -6,12 +12,24 @@ tmux -2 new-session -d -s $SESSION
 
 tmux new-window -t $SESSION:1 -k -n main
 tmux send-keys -t $SESSION:1 'vim Utils.h' C-m
-tmux send-keys -t $SESSION:1 ':' 'tabe ../dists.cfg' C-m
-tmux send-keys -t $SESSION:1 '1gt' C-m
+if [ $option -ne 1 ]
+then
+    tmux send-keys -t $SESSION:1 ':' 'tabe Router.h' C-m
+    tmux send-keys -t $SESSION:1 ':' 'tabe Router.c++' C-m
+else
+    tmux send-keys -t $SESSION:1 ':' 'tabe ../profile.cfg' C-m
+    tmux send-keys -t $SESSION:1 '1gt' C-m
+fi
 tmux split-window -h
-tmux send-keys -t $SESSION:1 'vim Router.h' C-m
-tmux send-keys -t $SESSION:1 ':' 'tabe Router.c++' C-m
-tmux select-pane -t 0
+if [ $option -eq 1 ]
+then
+    tmux send-keys -t $SESSION:1 'vim Router.h' C-m
+    tmux send-keys -t $SESSION:1 ':' 'tabe Router.c++' C-m
+    tmux select-pane -t 0
+else
+    tmux send-keys -t $SESSION:1 'vim Graph.h' C-m
+    tmux send-keys -t $SESSION:1 ':' 'tabe Graph.c++' C-m
+fi
 
 cd ..
 tmux new-window -t $SESSION:2 -n makefile
@@ -21,17 +39,24 @@ tmux send-keys -t $SESSION:2 ':' 'tabe tmux-start.bash' C-m
 tmux send-keys -t $SESSION:2 ':' 'tabe CMakeLists.txt' C-m
 tmux send-keys -t $SESSION:2 ':' 'tabe README.md' C-m
 tmux split-window -h
+if [ $option -ne 1 ]
+then
+    tmux send-keys -t $SESSION:2 'git st' C-m
+fi
 tmux select-pane -t 0
 
-cd ./extract_profiles/
-tmux new-window -t $SESSION:3 -n extract_profile
+if [ $option -eq 1 ]
+then
+    cd ./extract_profiles/
+    tmux new-window -t $SESSION:3 -n extract_profile
 
-tmux select-window -t $SESSION:3
-tmux send-keys -t $SESSION:3 'vim extract.c++' C-m
-tmux split-window -h
-tmux send-keys -t $SESSION:3 'vim Makefile' C-m
-tmux split-window -v
-tmux select-pane -t 0
+    tmux select-window -t $SESSION:3
+    tmux send-keys -t $SESSION:3 'vim extract.c++' C-m
+    tmux split-window -h
+    tmux send-keys -t $SESSION:3 'vim Makefile' C-m
+    tmux split-window -v
+    tmux select-pane -t 0
+fi
 
 tmux select-window -t $SESSION:1
 
